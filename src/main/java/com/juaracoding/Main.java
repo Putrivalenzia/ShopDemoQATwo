1736ab9736252431d8b05911c2f2880fefb355c0
PutriTarigan <putrivalenzia14@gmail.com>
2023-08-14 01:02:15 +0700

    Update main page

--- src/main/java/com/juaracoding/Main.java
+++ src/main/java/com/juaracoding/Main.java
@@ -1,19 +1,42 @@
 package com.juaracoding;
 
+import com.juaracoding.drivers.DriverSingleton;
+import com.juaracoding.pages.CartPage;
+import com.juaracoding.pages.CheckoutPage;
+import com.juaracoding.pages.LoginPage;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    static WebDriver driver;
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        DriverSingleton.getInstance("chrome");
+        driver = DriverSingleton.getDriver();
+        driver.get("https://shop.demoqa.com/my-account/");
+        WebDriver driver = new ChromeDriver();
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        LoginPage loginPage = new LoginPage();
+        loginPage.login();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        CartPage cartPage = new CartPage();
+        cartPage.addToCart();
+
+        CheckoutPage checkoutPage = new CheckoutPage();
+        checkoutPage.proceedCheckout();
+
+
+        driver.quit();
+
+        DriverSingleton.delay(0);
+        DriverSingleton.closeObjectInstance();
     }
 }
